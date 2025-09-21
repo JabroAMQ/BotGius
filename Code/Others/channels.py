7ea577a8c9432dcd891316d535507b3d7a29ba4c
JabroAMQ <jabrothegamer@gmail.com>
2025-09-21 19:17:50 +0200

    added notes for when fully moved to new server

--- Code/Others/channels.py
+++ Code/Others/channels.py
@@ -22,15 +22,15 @@ class Channels:
 
         self.main_guild_id = channels_data['main']['guild']
         self.test_guild_id = channels_data['test']['guild']
-        self.new_guild_id = channels_data['new_server']['guild']
+        self.new_guild_id = channels_data['new_server']['guild']        # NOTE delete when moved
 
         self.host_channel_id = channels_data['main']['host_chat']
         self.feedback_channel_id = channels_data['test']['feedback']
         self.report_channel_id = channels_data['test']['report']
         self.picks_channel_id = channels_data['test']['picks']
 
-        self.tour_announcements_channel_id = channels_data['new_server']['tour_announcements']['channel']
-        self.tour_announcements_message_id = channels_data['new_server']['tour_announcements']['message']
+        self.tour_announcements_channel_id = channels_data['new_server']['tour_announcements']['channel']           # NOTE change to main when moved
+        self.tour_announcements_message_id = channels_data['new_server']['tour_announcements']['message']           # NOTE change to main when moved
         self.test_tour_announcements_channel_id = channels_data['test']['tour_announcements']['channel']
         self.test_tour_announcements_message_id = channels_data['test']['tour_announcements']['message']
         
@@ -56,7 +56,7 @@ class Channels:
         """Return the test guild object."""
         return client.get_guild(self.test_guild_id)
     
-    def get_new_guild(self, client: discord.Client) -> discord.Guild:
+    def get_new_guild(self, client: discord.Client) -> discord.Guild:       # NOTE Remove when moved
         """Return the new guild object."""
         return client.get_guild(self.new_guild_id)
     
@@ -65,7 +65,7 @@ class Channels:
         match guild_id:
             case self.main_guild_id:
                 return self.get_main_guild(client)
-            case self.new_guild_id:
+            case self.new_guild_id:                                         # NOTE Remove when moved
                 return self.get_new_guild(client)
             case self.test_guild_id:
                 return self.get_test_guild(client)
@@ -96,7 +96,7 @@ class Channels:
 
     def get_main_tour_announcements_channel(self, client: discord.Client) -> discord.TextChannel:
         """Return the tour announcements channel object (from the main guild)."""
-        new_guild = self.get_new_guild(client)
+        new_guild = self.get_new_guild(client)      # NOTE Change to Main for consistance when fully moved
         return new_guild.get_channel(self.tour_announcements_channel_id)
 
     def get_main_tour_announcements_message(self, client: discord.Client) -> discord.Message:
@@ -117,7 +117,7 @@ class Channels:
     def get_tour_announcements_message(self, client: discord.Client, guild_id: int) -> discord.Message:
         """Return the tour announcements message object (from the main/test guild depending on the `guild_id` value)."""
         match guild_id:
-            case self.new_guild_id:
+            case self.new_guild_id:         # NOTE Change to main when moved
                 return self.get_main_tour_announcements_message(client)
             case self.test_guild_id:
                 return self.get_test_tour_announcements_message(client)
